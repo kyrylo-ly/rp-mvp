c68b50208398b10d80b364137b3561a9ec548d34
Kyrylo <kyrylo.lytvishko@gmail.com>
2026-08-08 14:21:54 +0300

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import Link from "next/link";
-
-export const CARS = [
-  { id: "1", name: "Porsche" },
-  { id: "2", name: "BMW" },
-  { id: "3", name: "Audi" },
-];
-
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Home Page Garage
-      <div>
-        {CARS.map((car) => (
-          <Link
-            className="p-6 hover:text-blue-500"
-            key={car.id}
-            href={`/photo/${car.id}`}
-          >
-            {car.name}
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+  return <div>Home Page</div>;
 }
