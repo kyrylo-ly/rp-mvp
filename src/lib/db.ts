0ff082ecd716bc132ed581062e98704df1a53a59
Kyrylo <kyrylo.lytvishko@gmail.com>
2026-08-08 20:03:32 +0300

    feat: instance db

---
+++ src/lib/db.ts
@@ -0,0 +1,28 @@
+import "server-only";
+
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+
+import * as schema from "@/db/schema";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) throw new Error("DATABASE_URL is missing in .env file");
+
+const globalForDb = globalThis as unknown as {
+  client: postgres.Sql | undefined;
+};
+
+export const client =
+  globalForDb.client ??
+  postgres(connectionString, {
+    max: process.env.NODE_ENV === "production" ? 1 : 10,
+    idle_timeout: 30,
+    connect_timeout: 5000,
+  });
+
+if (process.env.NODE_ENV === "development") {
+  globalForDb.client = client;
+}
+
+export const db = drizzle(client, { schema });
